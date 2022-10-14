beb18247c2fd1b8de2b89f4a214b8b9919838328
juleberry <105573843+juleberry@users.noreply.github.com>
2022-10-14 10:45:19 -0400

    setup controllers models, initial route setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import NavBar from './components/NavBar';
-import SignUpForm from './components/SignUpForm';
+import Home from './pages/Auth'
 // import { Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <h1>Spice Tracker</h1>
-      <SignUpForm />
+      <Home />
       {/* <Routes>
         <Route path="/" element={<ElementName />} />
         <Route path="/" element={<Element2Name />} />
