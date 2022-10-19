57e1878fdf7595b49cd98bd6c91b50192d92272c
juleberry <105573843+juleberry@users.noreply.github.com>
2022-10-19 13:14:42 -0400

    update controllers and routes; more spice function

--- controllers/api/spices.js
+++ controllers/api/spices.js
@@ -1,8 +1,25 @@
 const Spice = require('../../models/Spice')
 
-Spice.create(req.body, (error, createdSpice) => {
+const createSpice = (req, res) => {
+  Spice.create(req.body, (error, createdSpice) => {
   res.redirect('/spices')
   console.log('Spice Created')
 })
+}
 
-module.exports = { create }
+const editSpice = (req, res) => {
+  Spice.findOne({
+    _id: req.params.id
+  }, (error, foundSpice) => {
+    if (error) {
+      console.error(error);
+      res.json({
+        error: error
+      })
+    } else {
+      res.render('spices/edit', { spice: foundSpice });
+    }
+  })
+}
+
+module.exports = { createSpice }
