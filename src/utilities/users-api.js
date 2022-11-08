e56f8138197c224d4c2bfb88b3f9efdaaed56df6
juleberry <105573843+juleberry@users.noreply.github.com>
2022-11-08 10:50:13 -0500

    minor changes

--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const BASE_URL = 'https://spice-tracker-jb.herokuapp.com/';
+const BASE_URL = 'https://spice-tracker-jb.herokuapp.com/api/users';
 
 export async function signUp(userData) {
   try {
